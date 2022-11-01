df4e2a1cd3dbd347bf49eba8f80ff622875aa622
Ravaruc987 <speakonemore@gmail.com>
2022-11-01 18:07:25 +0000

    Add project files.

---
+++ ConsoleApp67/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleApp67
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerBase = 5;
+            int playerHp = 100;
+            int playerdamage = 45;
+
+            int enemyBase = 5;
+            int enemyHp = 50;
+            int enemydamage = 45;
+            {
+                Console.WriteLine("Wellcome let's player Game");
+            }
+            {
+                Console.ReadLine();
+            }       
+            Random random = new Random();
+            Int32 random4 = 0;
+            while (true)
+            {
+                Console.WriteLine("you win!!!");
+                Console.ReadKey();
+                Console.WriteLine("you lose have another try ok");
+            }
+            while (true)
+            {
+                int playerMp = 100;
+                int playerClass = 7;
+            }
+        }
+    }
+}
